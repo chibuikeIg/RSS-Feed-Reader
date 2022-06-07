a412411bce67b02a3003b1c2994b781638e68151
chibuikeIg <paulchibiukeigweze@gmail.com>
2022-06-07 15:40:28 +0100

    background service complete implementation

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,22 +1,29 @@
 package controllers
 
 import (
+	"context"
 	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"text/template"
 	"time"
 	"unicode"
 	"unicode/utf8"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chibuikeIg/Rss_blog/config"
 	"github.com/chibuikeIg/Rss_blog/models"
 	strip "github.com/grokify/html-strip-tags-go"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Rss struct {
@@ -159,16 +166,83 @@ func findAndStoreFeeds() {
 		diff := t2.Sub(t1)
 		last_poll, _ := strconv.Atoi(time.Time{}.Add(diff).Format("4"))
 		polling_frequency, _ := strconv.Atoi(settings[0].Polling_frequency)
-
+		fmt.Println("started crawling", last_poll)
 		if last_poll >= polling_frequency {
 
-			// crawl for feeds here
+			// get feeds here
 
-			fmt.Println(last_poll)
+			cursor, err := DB.Collection("feeds").Find(DB.Ctx, bson.M{}, options.Find().SetSort(bson.D{{"created_at", -1}}))
+
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			var feeds []models.Feed
+
+			if err = cursor.All(DB.Ctx, &feeds); err != nil {
+				log.Fatalln(err)
+			}
+
+			// get latest posts from feeds
+
+			for _, feed := range feeds {
+
+				var feed_posts []interface{}
+
+				response, _ := http.Get(feed.Link)
+
+				byteValue, _ := ioutil.ReadAll(response.Body)
+
+				rss := Rss{}
+
+				xml.Unmarshal(byteValue, &rss)
+
+				for _, item := range rss.Channel.Items {
+
+					var result bson.M
+					err = DB.Collection("posts").FindOne(context.TODO(), bson.D{{"slug", item.Link}}).Decode(&result)
+					if err == mongo.ErrNoDocuments {
+
+						cover := "n/a"
+
+						doc, _ := goquery.NewDocumentFromReader(strings.NewReader(item.Description))
+
+						if val, err := doc.Find("img").First().Attr("src"); err == true {
+							cover = val
+						}
+
+						post := models.Post{
+							Feed_id:     feed.Id,
+							Cover:       cover,
+							Title:       item.Title,
+							Slug:        item.Link,
+							Description: item.Description,
+							Author:      item.Creator,
+							Pub_date:    item.PubDate,
+							Created_at:  time.Now(),
+						}
+
+						feed_posts = append(feed_posts, post)
+
+					}
+
+				}
+
+				_, err = DB.Collection("posts").InsertMany(DB.Ctx, feed_posts)
+
+			}
+
+			// update last time polled
+
+			_, err = DB.Collection("settings").UpdateOne(context.TODO(), bson.D{{"_id", settings[0].Id}}, bson.D{{"$set", models.Setting{
+				Last_poll: time.Now(),
+			}}})
+
 		}
 
 	}
 
+	fmt.Println("background service running...")
 	return
 }
 
